3a2a26717a4ad7f743370da56d32056a55d27e4c
sayali_patil <sayalipatil27012005@gmail.com>
2026-03-29 18:10:36 +0530

    url changed

--- frontend/src/components/admin/PostJob.jsx
+++ frontend/src/components/admin/PostJob.jsx
@@ -42,7 +42,7 @@ const PostJob = () => {
         e.preventDefault();
         try {
             setLoading(true);
-            const res = await axios.post(`${JOB_API_END_POINT}/post`, input,{
+            const res = await axios.post(`https://campus-job-portal-26y4.onrender.com/api/v1/job/post`, input,{
                 headers:{
                     'Content-Type':'application/json'
                 },
