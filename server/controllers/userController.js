de30e3a9636225ba74d803558cdb3be2a8e61c1c
sayali_patil <sayalipatil27012005@gmail.com>
2026-03-29 18:47:45 +0530

    url changed

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -93,7 +93,7 @@ export const login = async (req, res) => {
             profile: user.profile
         }
 
-        return res.status(200).cookie("token", token, { maxAge: 1 * 24 * 60 * 60 * 1000, httpOnly: true,secure: true, sameSite: 'none' }).json({
+        return res.status(200).cookie("token", token, { maxAge: 1 * 24 * 60 * 60 * 1000, httpsOnly: true, sameSite: 'strict' }).json({
             message: `Welcome back ${user.fullname}`,
             user,
             success: true
@@ -104,7 +104,7 @@ export const login = async (req, res) => {
 }
 export const logout = async (req, res) => {
     try {
-        return res.status(200).cookie("token", "", { maxAge: 0, httpOnly: true,secure: true, sameSite: 'none' }).json({
+        return res.status(200).cookie("token", "", { maxAge: 0 }).json({
             message: "Logged out successfully.",
             success: true
         })
